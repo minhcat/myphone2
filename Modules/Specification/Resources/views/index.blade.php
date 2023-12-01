d01beae1fdc1579d83a079d03382dbc22799ee6d
Minh Cat <taminhcatcn12@gmail.com>
2023-12-01 23:51:38 +0700

    feature: make module - specification

---
+++ Modules/Specification/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('specification::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('specification.name') !!}
+    </p>
+@endsection
