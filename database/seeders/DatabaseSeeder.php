5840d46dd5fe0298ba6717d0b0a5f2ec88ace380
Minh Cat <taminhcatcn12@gmail.com>
2024-02-01 15:52:47 +0700

    feat: make migration, seeder tag

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SpecificationSeeder::class);
         $this->call(DetailSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(TagSeeder::class);
     }
 }
