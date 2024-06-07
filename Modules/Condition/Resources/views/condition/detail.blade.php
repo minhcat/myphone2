e9f7b9999811c3acf546ee97ad631974ceef37f6
Minh Cat <taminhcatcn12@gmail.com>
2024-06-07 22:51:10 +0700

    upd: change detail view - condition

--- Modules/Condition/Resources/views/condition/detail.blade.php
+++ Modules/Condition/Resources/views/condition/detail.blade.php
@@ -65,10 +65,10 @@
                 <div class="field-group">
                     <div class="row">
                         <div class="col-lg-2">
-                            <p><strong>Handler</strong></p>
+                            <p><strong>Type</strong></p>
                         </div>
                         <div class="col-lg-10">
-                            <p>Check Total Invoice</p>
+                            <p>{!! generate_label($condition->type, new App\Enums\ConditionType) !!}</p>
                         </div>
                     </div>
                 </div>
