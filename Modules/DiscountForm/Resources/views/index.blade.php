f02a35b273de806dc9283f15d60c131d2728089e
Minh Cat <taminhcatcn12@gmail.com>
2024-06-08 16:05:36 +0700

    feat: init discount form module - update todo

---
+++ Modules/DiscountForm/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('discountform::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('discountform.name') !!}
+    </p>
+@endsection
