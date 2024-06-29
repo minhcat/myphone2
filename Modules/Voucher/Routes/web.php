a1f4dce6d69ebb5e2fb79440e957ef2160d892df
Minh Cat <taminhcatcn12@gmail.com>
2024-06-29 17:12:53 +0700

    feat: add route voucher

--- Modules/Voucher/Routes/web.php
+++ Modules/Voucher/Routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::prefix('voucher')->group(function() {
-    Route::get('/', 'VoucherController@index');
+use Illuminate\Support\Facades\Route;
+use Modules\Voucher\Http\Controllers\VoucherController;
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::prefix('vouchers')->name('voucher.')->group(function() {
+        Route::get('/', [VoucherController::class, 'index'])->name('index');
+        Route::get('/create', [VoucherController::class, 'create'])->name('create');
+        Route::get('/{id}', [VoucherController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [VoucherController::class, 'edit'])->name('edit');
+        Route::post('/', [VoucherController::class, 'store'])->name('store');
+        Route::put('/{id}', [VoucherController::class, 'update'])->name('update');
+        Route::delete('/{id}', [VoucherController::class, 'destroy'])->name('delete');
+    });
 });
