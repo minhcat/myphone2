059a6757fe98464543ab7b4a6698040843d32e38
Minh Cat <taminhcatcn12@gmail.com>
2024-07-02 17:15:08 +0700

    feat: make route gift

--- Modules/Gift/Routes/web.php
+++ Modules/Gift/Routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::prefix('gift')->group(function() {
-    Route::get('/', 'GiftController@index');
+use Illuminate\Support\Facades\Route;
+use Modules\Gift\Http\Controllers\GiftController;
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::prefix('gifts')->name('gift.')->group(function() {
+        Route::get('/', [GiftController::class, 'index'])->name('index');
+        Route::get('/create', [GiftController::class, 'create'])->name('create');
+        Route::get('/{id}', [GiftController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [GiftController::class, 'edit'])->name('edit');
+        Route::post('/', [GiftController::class, 'store'])->name('store');
+        Route::put('/{id}', [GiftController::class, 'update'])->name('update');
+        Route::delete('/{id}', [GiftController::class, 'destroy'])->name('delete');
+    });
 });
