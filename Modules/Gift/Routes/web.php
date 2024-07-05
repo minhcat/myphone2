f3942e419be3e2694e1a9c86b26c22d360e0b8f5
Minh Cat <taminhcatcn12@gmail.com>
2024-07-05 10:58:41 +0700

    feat: make controller and add route - gift product item

--- Modules/Gift/Routes/web.php
+++ Modules/Gift/Routes/web.php
@@ -14,13 +14,22 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Gift\Http\Controllers\GiftController;
 use Modules\Gift\Http\Controllers\GiftProductController;
+use Modules\Gift\Http\Controllers\GiftProductItemController;
 
 Route::prefix('admin')->name('admin.')->group(function() {
     Route::prefix('gifts')->name('gift.')->group(function() {
-        Route::prefix('/{gift_id}/product')->name('product.')->group(function() {
+        Route::prefix('/{gift_id}/products')->name('product.')->group(function() {
+            Route::prefix('/{gift_product_id}/items')->name('item.')->group(function() {
+                Route::get('/', [GiftProductItemController::class, 'index'])->name('index');
+                Route::get('/create', [GiftProductItemController::class, 'create'])->name('create');
+                Route::get('/{id}/edit', [GiftProductItemController::class, 'edit'])->name('edit');
+                Route::post('/', [GiftProductItemController::class, 'store'])->name('store');
+                Route::put('/{id}', [GiftProductItemController::class, 'update'])->name('update');
+                Route::delete('/{id}', [GiftProductItemController::class, 'destroy'])->name('delete');
+            });
+
             Route::get('/', [GiftProductController::class, 'index'])->name('index');
             Route::get('/create', [GiftProductController::class, 'create'])->name('create');
-            Route::get('/{id}', [GiftProductController::class, 'show'])->name('show');
             Route::get('/{id}/edit', [GiftProductController::class, 'edit'])->name('edit');
             Route::post('/', [GiftProductController::class, 'store'])->name('store');
             Route::put('/{id}', [GiftProductController::class, 'update'])->name('update');
