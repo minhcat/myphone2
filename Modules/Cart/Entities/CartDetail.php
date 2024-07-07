8caecffb56a85e46196820384522c8454505af63
Minh Cat <taminhcatcn12@gmail.com>
2024-07-07 16:17:04 +0700

    ref: update model change product to target - cart, order, invoice

--- Modules/Cart/Entities/CartDetail.php
+++ Modules/Cart/Entities/CartDetail.php
@@ -2,17 +2,27 @@
 
 namespace Modules\Cart\Entities;
 
+use App\Enums\TargetType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Product\Entities\Product;
+use Modules\Product\Entities\Variation;
 
 class CartDetail extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['cart_id', 'product_id', 'price', 'quantity'];
+    protected $fillable = ['cart_id', 'target_type', 'target_id', 'price', 'quantity'];
 
     public $timestamps = false;
+
+    public function target()
+    {
+        if ($this->target_type === TargetType::VARIANT) {
+            return $this->belongsTo(Variation::class, 'target_id');
+        }
+        return $this->belongsTo(Product::class, 'target_id');
+    }
 
     public function cart()
     {
--- Modules/Invoice/Entities/InvoiceDetail.php
+++ Modules/Invoice/Entities/InvoiceDetail.php
@@ -2,17 +2,27 @@
 
 namespace Modules\Invoice\Entities;
 
+use App\Enums\TargetType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Product\Entities\Product;
+use Modules\Product\Entities\Variation;
 
 class InvoiceDetail extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['invoice_id', 'product_id', 'price', 'quantity'];
+    protected $fillable = ['invoice_id', 'target_type', 'target_id', 'price', 'quantity'];
 
     public $timestamps = false;
+
+    public function target()
+    {
+        if ($this->target_type === TargetType::VARIANT) {
+            return $this->belongsTo(Variation::class, 'target_id');
+        }
+        return $this->belongsTo(Product::class, 'target_id');
+    }
 
     public function product()
     {
