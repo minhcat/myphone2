318418e26f70d0beb652c6bf33f4c16168ff88f4
Minh Cat <taminhcatcn12@gmail.com>
2024-07-09 15:03:53 +0700

    feat: add route - city

--- Modules/City/Routes/web.php
+++ Modules/City/Routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::prefix('city')->group(function() {
-    Route::get('/', 'CityController@index');
+use Illuminate\Support\Facades\Route;
+use Modules\City\Http\Controllers\CityController;
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::prefix('city')->group(function() {
+        Route::get('/', [CityController::class, 'index'])->name('index');
+        Route::get('/create', [CityController::class, 'create'])->name('create');
+        Route::get('/{id}', [CityController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [CityController::class, 'edit'])->name('edit');
+        Route::post('/', [CityController::class, 'store'])->name('store');
+        Route::put('/{id}', [CityController::class, 'update'])->name('update');
+        Route::delete('/{id}', [CityController::class, 'destroy'])->name('delete');
+    });
 });
