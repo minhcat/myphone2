424817a562755fa8d0a59f35112a3cc86fbd0e3e
Minh Cat <taminhcatcn12@gmail.com>
2024-07-12 10:42:46 +0700

    feat: make controller, route - ward city

--- Modules/City/Routes/web.php
+++ Modules/City/Routes/web.php
@@ -14,10 +14,21 @@
 use Illuminate\Support\Facades\Route;
 use Modules\City\Http\Controllers\CityController;
 use Modules\City\Http\Controllers\DistrictController;
+use Modules\City\Http\Controllers\WardController;
 
 Route::prefix('admin')->name('admin.')->group(function() {
     Route::prefix('cities')->name('city.')->group(function() {
         Route::prefix('/{city_id}/districts')->name('district.')->group(function() {
+            Route::prefix('/{district_id}/wards')->name('ward.')->group(function() {
+                Route::get('/', [WardController::class, 'index'])->name('index');
+                Route::get('/create', [WardController::class, 'create'])->name('create');
+                Route::get('/{id}', [WardController::class, 'show'])->name('show');
+                Route::get('/{id}/edit', [WardController::class, 'edit'])->name('edit');
+                Route::post('/', [WardController::class, 'store'])->name('store');
+                Route::put('/{id}', [WardController::class, 'update'])->name('update');
+                Route::delete('/{id}', [WardController::class, 'destroy'])->name('delete');
+            });
+    
             Route::get('/', [DistrictController::class, 'index'])->name('index');
             Route::get('/create', [DistrictController::class, 'create'])->name('create');
             Route::get('/{id}', [DistrictController::class, 'show'])->name('show');
