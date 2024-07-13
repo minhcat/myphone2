6219622674928b145b4caa049fd435c2a5a94dbd
Minh Cat <taminhcatcn12@gmail.com>
2024-07-13 16:20:48 +0700

    feat: add route - area

--- Modules/Area/Routes/web.php
+++ Modules/Area/Routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::prefix('area')->group(function() {
-    Route::get('/', 'AreaController@index');
+use Illuminate\Support\Facades\Route;
+use Modules\Area\Http\Controllers\AreaController;
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::prefix('areas')->name('area.')->group(function() {
+        Route::get('/', [AreaController::class, 'index'])->name('index');
+        Route::get('/create', [AreaController::class, 'create'])->name('create');
+        Route::post('/', [AreaController::class, 'store'])->name('store');
+        Route::get('/{id}', [AreaController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [AreaController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [AreaController::class, 'update'])->name('update');
+        Route::delete('/{id}', [AreaController::class, 'destroy'])->name('delete');
+    });
 });
