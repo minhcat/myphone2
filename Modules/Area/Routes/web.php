564f619a5d12594d7618b350bcb9325b6444399b
Minh Cat <taminhcatcn12@gmail.com>
2024-07-15 10:42:41 +0700

    feat: make controller, add route - area detail

--- Modules/Area/Routes/web.php
+++ Modules/Area/Routes/web.php
@@ -13,9 +13,20 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Area\Http\Controllers\AreaController;
+use Modules\Area\Http\Controllers\AreaDetailController;
 
 Route::prefix('admin')->name('admin.')->group(function() {
     Route::prefix('areas')->name('area.')->group(function() {
+        Route::prefix('{area_id}/detail')->name('detail.')->group(function() {
+            Route::get('/', [AreaDetailController::class, 'index'])->name('index');
+            Route::get('/create', [AreaDetailController::class, 'create'])->name('create');
+            Route::post('/', [AreaDetailController::class, 'store'])->name('store');
+            Route::get('/{id}', [AreaDetailController::class, 'show'])->name('show');
+            Route::get('/{id}/edit', [AreaDetailController::class, 'edit'])->name('edit');
+            Route::put('/{id}', [AreaDetailController::class, 'update'])->name('update');
+            Route::delete('/{id}', [AreaDetailController::class, 'destroy'])->name('delete');
+        });
+
         Route::get('/', [AreaController::class, 'index'])->name('index');
         Route::get('/create', [AreaController::class, 'create'])->name('create');
         Route::post('/', [AreaController::class, 'store'])->name('store');
