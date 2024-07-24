bbf763daae2df2f0f42d3d5438a5aa9ba161db04
Minh Cat <taminhcatcn12@gmail.com>
2024-07-24 12:05:51 +0700

    feat: make controller, add route - user address

--- Modules/User/Routes/web.php
+++ Modules/User/Routes/web.php
@@ -12,10 +12,20 @@
 */
 
 use Illuminate\Support\Facades\Route;
+use Modules\User\Http\Controllers\AddressController;
 use Modules\User\Http\Controllers\UserController;
 
 Route::prefix('admin')->name('admin.')->group(function() {
     Route::prefix('users')->name('user.')->group(function() {
+        Route::prefix('{user_id}/addresses')->name('address.')->group(function() {
+            Route::get('/', [AddressController::class, 'index'])->name('index');
+            Route::get('/create', [AddressController::class, 'create'])->name('create');
+            Route::get('/{id}', [AddressController::class, 'show'])->name('show');
+            Route::get('/{id}/edit', [AddressController::class, 'edit'])->name('edit');
+            Route::post('/', [AddressController::class, 'store'])->name('store');
+            Route::put('/{id}', [AddressController::class, 'update'])->name('update');
+            Route::delete('/{id}', [AddressController::class, 'destroy'])->name('delete');
+        });
         Route::get('/', [UserController::class, 'index'])->name('index');
         Route::get('/create', [UserController::class, 'create'])->name('create');
         Route::get('/{id}', [UserController::class, 'show'])->name('show');
