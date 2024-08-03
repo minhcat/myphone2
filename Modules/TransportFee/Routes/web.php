e47b1bc95902afc993c5336a02fb52c8b9649599
Minh Cat <taminhcatcn12@gmail.com>
2024-08-03 22:08:07 +0700

    ref: add route transport fee area case - transport fee - make controller

--- Modules/TransportFee/Routes/web.php
+++ Modules/TransportFee/Routes/web.php
@@ -12,16 +12,25 @@
 */
 
 use Illuminate\Support\Facades\Route;
+use Modules\TransportFee\Http\Controllers\TransportFeeAreaCaseController;
 use Modules\TransportFee\Http\Controllers\TransportFeeAreaController;
 use Modules\TransportFee\Http\Controllers\TransportFeeController;
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function() {
     Route::prefix('transport_fees')->name('transport_fee.')->group(function() {
         Route::prefix('{transport_fee_id}/areas')->name('area.')->group(function() {
+            Route::prefix('{transport_fee_area_id}/cases')->name('case.')->group(function() {
+                Route::get('/', [TransportFeeAreaCaseController::class, 'index'])->name('index');
+                Route::get('/create', [TransportFeeAreaCaseController::class, 'create'])->name('create');
+                Route::post('/', [TransportFeeAreaCaseController::class, 'store'])->name('store');
+                Route::get('/{id}/edit', [TransportFeeAreaCaseController::class, 'edit'])->name('edit');
+                Route::put('/{id}', [TransportFeeAreaCaseController::class, 'update'])->name('update');
+                Route::delete('/{id}', [TransportFeeAreaCaseController::class, 'destroy'])->name('delete');
+            });
+
             Route::get('/', [TransportFeeAreaController::class, 'index'])->name('index');
             Route::get('/create', [TransportFeeAreaController::class, 'create'])->name('create');
             Route::post('/', [TransportFeeAreaController::class, 'store'])->name('store');
-            Route::get('/{id}', [TransportFeeAreaController::class, 'show'])->name('show');
             Route::get('/{id}/edit', [TransportFeeAreaController::class, 'edit'])->name('edit');
             Route::put('/{id}', [TransportFeeAreaController::class, 'update'])->name('update');
             Route::delete('/{id}', [TransportFeeAreaController::class, 'destroy'])->name('delete');
