4b9065def2c18a8333f5f449d159f3b37230fa8c
Minh Cat <taminhcatcn12@gmail.com>
2024-08-26 15:19:15 +0700

    ref: remove code product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -132,22 +132,5 @@ class ProductSeeder extends Seeder
         //         'updated_at'    => now()->format('Y-m-d H:i:s'),
         //     ],
         // ];
-
-        // for ($i = 1; $i <= 30; $i++) {
-        //     $data[] = [
-        //         'name'          => 'smartphone ' . $i,
-        //         'slug'          => 'smartphone-' . $i,
-        //         'sku_prefix'    => 'PRO',
-        //         'sku_number'    => '00' . (20 + $i),
-        //         'price'         => 5000000,
-        //         'description'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        //         'brand_id'      => null,
-        //         'author_id'     => 1,
-        //         'created_at'    => now()->format('Y-m-d H:i:s'),
-        //         'updated_at'    => now()->format('Y-m-d H:i:s'),
-        //     ];
-        // }
-
-        // DB::table('products')->insert($data);
     }
 }
