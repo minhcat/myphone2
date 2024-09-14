29d41bb88caae3a8040a46720060bd9c92612f35
Minh Cat <taminhcatcn12@gmail.com>
2024-09-14 21:07:51 +0700

    feat: make information faker - faker

--- Modules/Specification/Entities/Information.php
+++ Modules/Specification/Entities/Information.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Specification\Entities;
 
+use Database\Factories\InformationFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\User\Entities\User;
@@ -17,5 +18,10 @@ class Information extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public static function newFactory()
+    {
+        return new InformationFactory();
     }
 }
